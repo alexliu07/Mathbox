05d0f995b7b6fa80739940bea82d198bd3e91aeb
Yuchen Liu <101923999+alexliu07@users.noreply.github.com>
2023-01-06 17:34:12 +0800

    Common Factor Finding UI Completed

--- app/src/main/java/com/alexliu07/mathbox/MainActivity.java
+++ app/src/main/java/com/alexliu07/mathbox/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_radSimp, R.id.nav_facDecomp, R.id.nav_slideshow)
+                R.id.nav_radSimp, R.id.nav_facDecomp, R.id.nav_comFacFind)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
