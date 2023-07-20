aecbb3abc65a9900249f1bae2ffb3748f272f969
Yuchen Liu <101923999+alexliu07@users.noreply.github.com>
2023-07-20 15:52:07 +0800

    Use GCD Improved Fraction Approximation

--- app/src/main/java/com/alexliu07/mathbox/function/FractionApproximation.java
+++ app/src/main/java/com/alexliu07/mathbox/function/FractionApproximation.java
@@ -1,8 +1,9 @@
 package com.alexliu07.mathbox.function;
 
-import java.util.ArrayList;
-
 public class FractionApproximation {
+    public static int gcd(int x,int y){
+        return y==0?x:gcd(y,x%y);
+    }
     public static int[] simp(double a,double b,int bitA,int bitB){
         int[] ints = new int[2];
         //乘一个大的位数
@@ -14,8 +15,7 @@ public class FractionApproximation {
             ints[1] = (int) (b*Math.pow(10,bitB));
         }
         //寻找最大公因数
-        ArrayList<Integer> comfac = CommonFactorFinding.findcomfac(ints[0],ints[1]);
-        int maxcomfac = comfac.get(comfac.size()-1);
+        int maxcomfac = gcd(ints[0],ints[1]);
         //除以最大公因数
         ints[0] /= maxcomfac;
         ints[1] /= maxcomfac;
